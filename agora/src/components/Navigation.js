739c65d2ef51db5f5636d19f77e21619aab86b46
chenwill98 <chenwill98@gmail.com>
2019-04-10 19:35:30 -0400

    Added more pages

--- agora/src/components/Navigation.js
+++ agora/src/components/Navigation.js
@@ -4,36 +4,6 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 
 export default class Navigation extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            cookie: "",
-            session: false,
-            error: false
-        };
-    }
-
-    //fetches the locally cached cookie and compares it to the DB's cookie
-    // componentDidMount() {
-    //     this.setState({cookie:localStorage.getItem('cookie')});
-    //     axios.get("http://localhost:8080/login", {
-    //         headers:{
-    //             Cookie: this.state.cookie
-    //         }
-    //     }).then(response => {
-    //         if (response.data == "Error") { //some error
-    //             this.setState({session:false})
-    //         } else {
-    //             this.setState({session:true})
-    //         }
-    //     })
-    // }
-
-    deleteCookie = () => {
-        localStorage.removeItem('cookie');
-        this.setState({cookie: ""});
-    }
 
     render() {
         return (
@@ -50,7 +20,7 @@ export default class Navigation extends Component {
                             <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item onClick={() =>
-                                    this.deleteCookie()
+                                    // this.deleteCookie()
                             }>Sign Out</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
