d291051f1b8c23b5c3640310c0a189725dd13bf6
RayhanSyed <rsyed528@gmail.com>
2019-04-16 10:48:23 -0400

    Create CreateGroup.js

---
+++ agora/src/containers/CreateGroup.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import { Form, Button, Card, Col, Alert } from "react-bootstrap";
+import { Redirect } from 'react-router-dom'
+import CenterView from '../components/CenterView.js';
