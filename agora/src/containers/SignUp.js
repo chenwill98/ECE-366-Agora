b9f849748e1292715b966d7d09400535387cf948
chenwill98 <chenwill98@gmail.com>
2019-04-17 17:53:57 -0400

    Additional styling for the welcome page

--- agora/src/containers/SignUp.js
+++ agora/src/containers/SignUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import { Form, Button, Card, Col, Alert } from "react-bootstrap";
-import "../styles/Login.css";
+import "../styles/SignUp.css";
 import { Redirect } from 'react-router-dom'
 import WelcomeNav from '../components/WelcomeNav.js';
 import CenterView from '../components/CenterView.js';
